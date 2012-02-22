bef68b6a6de9eacda2bb23db0d5e068a1330cc96
Fernandez Lucas <lfernandez@ckab.net>
2012-02-22 15:17:24 +0100

    adapt captor template to SPL

--- PolluxSensorSound/PolluxSensorSound.ino
+++ PolluxSensorSound/PolluxSensorSound.ino
@@ -1,10 +1,10 @@
 /*
-              +-\/-+
-    (RESET)  1|    |8  VCC (2.7-5.5V)
- Sound Read  2|    |7  I2C   SCK -> Uno A5
-      DEBUG  3|    |6  (PB1) NA
-        GND  4|    |5  I2C   SDA -> Uno A4
-              +----+ 
+               +-\/-+
+     (RESET)  1|    |8  VCC (2.7-5.5V)
+  Sound Read  2|    |7  I2C   SCK -> Uno A5
+       DEBUG  3|    |6  (PB1) NA
+         GND  4|    |5  I2C   SDA -> Uno A4
+               +----+ 
 */
 
 #include "TinyWireS.h"       // ATTiny wire lib
@@ -24,6 +24,55 @@ byte*     floatPtr;
 byte      byteRcvd;
 byte      i;
 
+// sensor  dbmeter
+// 680  == 345 == 90dB
+// 672  == 330 == 85dB
+// 620  == 415 == 80dB
+// 450? == 397 == 75dB
+// 420? == 390 == 70dB
+// 270  == 331 == 66dB
+
+byte convert_to_dB(float val) {
+    // 10.0 == 50dB
+    if (val<10.0)        return 50; // <50dB
+    // 15.0 == 55dB
+    else if (val<15.0)   return 55; // <55dB
+    // 30.0 == 60dB
+    else if (val<30.0)   return 60; // <60dB
+    // 70.0 == 65dB
+    else if (val<70.0)   return 65; // <65dB
+    // 140.0 == 70dB
+    else if (val<140.0)  return 70; // <70dB
+    // 230.0 == 75dB
+    else if (val<230.0)  return 75; // <75dB
+    // 250.0 == 80dB
+    else if (val<255.0)  return 80; // <80dB
+    // 255.0 == 85dB == 90 dB
+    else if (val<255.0)  return 85; // <85dB
+    else if (val>=255.0) return 90; // >85dB
+}
+
+const char* convert_to_dB_ascii(float val) {
+    // 10.0 == 50dB
+    if (val<11.0)        return "  <50dB"; // <50dB
+    // 15.0 == 55dB
+    else if (val<16.0)   return "50-55dB"; // <55dB
+    // 30.0 == 60dB
+    else if (val<31.0)   return "55-60dB"; // <60dB
+    // 70.0 == 65dB
+    else if (val<71.0)   return "60-65dB"; // <65dB
+    // 140.0 == 70dB
+    else if (val<141.0)  return "65-70dB"; // <70dB
+    // 230.0 == 75dB
+    else if (val<230.0)  return "70-75dB"; // <75dB
+    // 250.0 == 80dB
+    else if (val<251.0)  return "75-80dB"; // <80dB
+    // 255.0 == 85dB == 90 dB
+    else if (val<256.0)  return "80-85dB"; // <85dB
+    // and over...
+    else if (val>=256.0) return "  >85dB"; // >85dB
+}
+
 void setup() {
   pinMode(DEBUG,OUTPUT);
   pinMode(SOUND_READ,INPUT);
@@ -39,23 +88,15 @@ void setup() {
 void loop() {
     if (TinyWireS.available()){          // if we get an I2C message
     byteRcvd = TinyWireS.receive();      // do nothing with the message
-    
+
     for(i=0;i<50;i++) {                  // make 50 temperature sample
-      sound.add(analogRead(SOUND_READ));   // read and store temperature sensor output
+      sound.add(analogRead(SOUND_READ)); // read and store temperature sensor output
       delayMicroseconds(DELAY_ADC);      // wait 10 us to stabilise ADC
-//    digitalWrite(DEBUG,HIGH);
-//    digitalWrite(DEBUG,LOW);
+      digitalWrite(DEBUG,HIGH);
+      digitalWrite(DEBUG,LOW);
     }
 
-    result = ((sound.average()*0.0049)-0.5)*100 ; // convert the result into a humain readable output
-
-    if(result < 0) result = 0;           // avoid eratic datas
+    TinyWireS.send(convert_to_dB(sound.average()));
     sound.clear();                        // clear statistics to avoid leack and data stacking
-    
-    floatPtr = (byte*) &result;
-                TinyWireS.send( *floatPtr );  // send first byte
-    ++floatPtr; TinyWireS.send( *floatPtr );  // the second
-    ++floatPtr; TinyWireS.send( *floatPtr );  // third
-    ++floatPtr; TinyWireS.send( *floatPtr );  // fourth and final byte
   }
 }
--- PolluxWakeupduinoSound/PolluxWakeupduinoSound.ino
+++ PolluxWakeupduinoSound/PolluxWakeupduinoSound.ino
@@ -1,37 +1,34 @@
 #include <Wire.h>
 
-#define I2C_SLAVE_ADDR   0x26              // I2C Dust slave address
+#define I2C_SLAVE_ADDR   0x28              // I2C Sound slave address
 
-#define DELAY_DUST_READ    80              // wait 80 ms before ask dust value
+#define DELAY_SOUND_READ   140             // wait 140 ms before ask dust value
 #define DELAY_NEW_SAMPLE 1000              // time before new sample (1s)
 
-float dustValue;
-byte* floatPtr;
+byte soundValue;
 
 void setup(){
   Wire.begin();                            // start I2C Bus as Master
   Serial.begin(38400);                     // start serial to send dust output
-  dustValue = 0;
+  soundValue = 0;
 }
 
 void loop(){
-  dustValue = 0;
+  soundValue = 0;
   
   Wire.beginTransmission(I2C_SLAVE_ADDR);  // transmit to slave device
   Wire.write(0xFF);                        // sends usuless data 
   Wire.endTransmission();                  // stop transmitting
 
-  delay(DELAY_DUST_READ);
+  delay(DELAY_SOUND_READ);
 
-  Wire.requestFrom(I2C_SLAVE_ADDR, 4);     // request 4 byte from slave (float = 4 bytes)
+  Wire.requestFrom(I2C_SLAVE_ADDR, 1);     // request 1 byte from slave
 
   if (Wire.available()) {
-    floatPtr = (byte*) &dustValue;
-	(*floatPtr) = Wire.read(); ++floatPtr;
-	(*floatPtr) = Wire.read(); ++floatPtr;
-	(*floatPtr) = Wire.read(); ++floatPtr;
-	(*floatPtr) = Wire.read();
-	Serial.println(dustValue,DEC);     // print out slave byte to Serial monitor
+    soundValue = Wire.read();
+	Serial.print("Sound = ");              // print out slave byte to Serial monitor
+	Serial.print(soundValue,DEC);          // print out slave byte to Serial monitor
+	Serial.println(" dB");                 // print out slave byte to Serial monitor
   } else {
     Serial.println("NaN");  
   }
