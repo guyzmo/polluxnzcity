04b4acfb9769d46a8cb01ddaa2574a7a9632ac38
Guyzmo <guyzmo+github@m0g.net>
2012-03-01 12:45:56 +0100

    [ENH #625] reverted to XBee original library

--- PolluxWakeupduino/PolluxWakeupduino.ino
+++ PolluxWakeupduino/PolluxWakeupduino.ino
@@ -1,23 +1,100 @@
-#undef API_ESCAPED_MODE 
-#undef VERBOSE
+/**
+ * Copyright (c) 2009 Andrew Rapp. All rights reserved.
+ *
+ * This file is part of XBee-Arduino.
+ *
+ * XBee-Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBee-Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "../PolluxGateway/XbeeCommunicatorLight.h"
+#include <XBee.h>
 
-XbeeCommunicator xbeecom(1,0);
+/*
+This example is for Series 2 XBee
+ Sends a ZB TX request with the value of analogRead(pin5) and checks the status response for success
+*/
 
-uint8_t panid[2] = { 0x4, 0x2 };
-uint8_t venid[8] = {0x0, 0x0, 0x1, 0x3, 0xA, 0x2, 0x0, 0x0};
+// create the XBee object
+XBee xbee = XBee();
 
+uint8_t payload[] = { 0, 0 };
 
-void setup() {
-    xbeecom.begin(panid,venid);
-    pinMode(13, OUTPUT);
+// SH + SL Address of receiving XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x403e0f30);
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
+int pin5 = 0;
+
+int statusLed = 13;
+int errorLed = 13;
+
+void flashLed(int pin, int times, int wait) {
+
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+
+    if (i + 1 < times) {
+      delay(wait);
+    }
+  }
 }
 
-void loop() {
-    digitalWrite(13, HIGH);
-    xbeecom.send("caca!\n");
-    digitalWrite(13, LOW);
-    delay(1000);
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+
+  xbee.begin(9600);
+}
+
+void loop() {   
+  // break down 10-bit reading into two bytes and place in payload
+  payload[0] = 0x2;
+  payload[1] = 0xA;
+
+  xbee.send(zbTx);
+
+  // flash TX indicator
+  flashLed(statusLed, 1, 100);
+
+  // after sending a tx request, we expect a status response
+  // wait up to half second for the status response
+  if (xbee.readPacket(500)) {
+    // got a response!
+
+    // should be a znet tx status            	
+    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+      xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+      // get the delivery status, the fifth byte
+      if (txStatus.getDeliveryStatus() == SUCCESS) {
+        // success.  time to celebrate
+        flashLed(statusLed, 5, 50);
+      } else {
+        // the remote XBee did not receive our packet. is it powered on?
+        flashLed(errorLed, 3, 500);
+      }
+    }
+  } else if (xbee.getResponse().isError()) {
+    //nss.print("Error reading packet.  Error code: ");  
+    //nss.println(xbee.getResponse().getErrorCode());
+  } else {
+    // local XBee did not provide a timely TX Status Response -- should not happen
+    flashLed(errorLed, 2, 50);
+  }
+
+  delay(1000);
 }
 
