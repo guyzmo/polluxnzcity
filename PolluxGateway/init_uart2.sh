170dc834a6a770237925efe56a5b66eb078cc03f
Guyzmo <guyzmo+github@m0g.net>
2012-03-02 19:21:07 +0100

    added pollux gateway base code and init uart script

---
+++ PolluxGateway/init_uart2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo 1 > /sys/kernel/debug/omap_mux/spi0_d0
+echo 21 > /sys/kernel/debug/omap_mux/spi0_sclk
+
--- PolluxGateway/pollux_gateway.C
+++ PolluxGateway/pollux_gateway.C
@@ -15,84 +15,106 @@
 #include <string.h>     // memset
 #include <errno.h>
 #include <stdlib.h>
+#include <termios.h>
 #include <sys/epoll.h>
 
 #define EPOLL_MAX_CONN 2
 #define EPOLL_RUN_TIMEOUT -1
+#define BUFF_SIZE 50
 
 class Serial {
+    char* port;
+
+    int epfd;
+    int fd;
+    struct epoll_event events;
+
     public:
+        Serial(char* port) : port(port) {
+        }
 
-        Serial (char* port) {
-            int epfd = epoll_create(EPOLL_MAX_CONN);
-            int fd = open("/dev/ttyO2", O_RDWR | O_NONBLOCK);
-
-            if( fd > 0 ) {
-                const int BUFF_SIZE = 50;
-                char buff[BUFF_SIZE];
-                memset(buff, 0, BUFF_SIZE);
-
-                struct epoll_event ev;
-                struct epoll_event events;
-                ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP;
-                ev.data.fd = fd;
-
-                int res = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
-                if( res < 0 )
-                    printf("Error epoll_ctl: %i\n", errno);
+        int begin() {
+            int res;
+            struct epoll_event ev;
+            struct termios my_termios;
+            
+            fd = open(port, O_RDWR | O_NOCTTY | O_NONBLOCK);
+            if (fd == -1) {
+                perror("Unable to open serial port.");
+                return -1;
             }
+
+            /* Thanks to Milkymist project to have found Julien Schmitt's work (GTKTerm) 
+             * for figuring out the correct parameters to put into that weird struct.
+             */
+            tcgetattr(fd, &my_termios);
+            my_termios.c_cflag = B9600;
+            my_termios.c_cflag |= CS8;
+            my_termios.c_cflag |= CREAD;
+            my_termios.c_iflag = IGNPAR | IGNBRK;
+            my_termios.c_cflag |= CLOCAL;
+            my_termios.c_oflag = 0;
+            my_termios.c_lflag = 0;
+            my_termios.c_cc[VTIME] = 0;
+            my_termios.c_cc[VMIN] = 1;
+            tcsetattr(fd, TCSANOW, &my_termios);
+            tcflush(fd, TCOFLUSH);
+            tcflush(fd, TCIFLUSH);
+
+            epfd = epoll_create(EPOLL_MAX_CONN);
+            ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP;
+            ev.data.fd = fd;
+
+            res = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
+            if( res < 0 ) {
+                printf("Error epoll_ctl: %i\n", errno);
+                return -1;
+            }
+            return 0;
         }
 
         int poll() {
             int n = epoll_wait(epfd, &events, EPOLL_MAX_CONN, EPOLL_RUN_TIMEOUT);
 
-            printf("Epoll unblocked\n");
             if(n < 0)
-                perror("Epoll failed\n");
+                perror("Epoll failed");
             else if(n==0)
                 printf("TIMEOUT\n");
-            else
-            {
-                int size = read(fd, buff, BUFF_SIZE-1);
-                if( size < 0 )
-                    printf("Error Reading the device: %s\n", strerror(errno));
-                    return -1;
-                else if( size > 0 ) {
-                    printf("Input found!\n");
-                    buff[size] = '\0';
-                    printf("%x\n", buff);
-                    return 1;
-                } else {
-                    printf("No input\n");
-                    return 0;
-                }
-
-                if( errno == EAGAIN ) {
-                    printf("ERRNO: EAGAIN\n");
-                    return -2;
-                }
-                return -3;
+            else {
+                this->read();
             }
         }
 
-        virtual int read() {
-            
-            
-        };
+        void read() {
+            int size;
+            char buff[BUFF_SIZE];
+            memset(buff, 0, BUFF_SIZE);
 
-        virtual int write(int val) {
+            size = ::read(fd, buff, BUFF_SIZE);
+            if( size < 0 )
+                printf("Error Reading the device: %s\n", strerror(errno));
+            else if( size > 0 ) {
+                printf("Input: %x\n", *buff);
+            } else
+                printf("No input\n");
 
+            if (errno == EAGAIN)
+                perror("ERRNO: EAGAIN\n");
+        }
+
+        void write(char* data, int len) {
+            ::write(fd, data, len);
         }
 };
 
+int main(void) {
+    Serial s = Serial("/dev/ttyO2");
+    if (s.begin() == 0);
+        char data[] = {'+', '+', '+', 'A','T','N','D','\r'};
+        s.write(data, 8);
+        for (;;)
+            s.poll();
+    return 0;
+}
 
-int main() {
-    //XbeeCommunicator xbeecom("/dev/ttyO2");
-    //xbeecom.begin(panid,venid);
-    
-    Serial xbeecom = Serial("/dev/ttyO2");
 
-    while (xbeecom.poll() > 0) {
-        printf("%s", xbeecom.read());
-    }
-};
