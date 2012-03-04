58846d772e6a95a9299500e233c4fba4b9c0c2a1
Guyzmo <guyzmo+github@m0g.net>
2012-03-04 14:35:52 +0100

    [ENH #625] added beaglebone led features and enabled them

---
+++ PolluxGateway/beaglebone.h
@@ -0,0 +1,4 @@
+#define GPIO0 0
+#define GPIO1 GPIO0+32
+#define GPIO2 GPIO1+32
+#define GPIO3 GPIO2+32
--- PolluxGateway/pollux_gateway.C
+++ PolluxGateway/pollux_gateway.C
@@ -1,28 +1,35 @@
-//#define API_ESCAPED_MODE 1
 //#define TIMING 50
 //#define VERBOSE 
-//#undef API_ESCAPED_MODE // if API MODE is 2, useful for use with software flow control
+//#define API_ESCAPED_MODE // if API MODE is 2, useful for use with software flow control
 
-//#include "XbeeCommunicatorLight.h"
-
-//#define VERBOSE 1
+#include "beaglebone.h"
 #include "xbee_communicator.h"
 
 const int panid[2] = {0x2,0xA};
 const int venid[8] = {0x0, 0x0, 0x1, 0x3, 0xA, 0x2, 0x0, 0x0};
 
 class XbeePollux : public XbeeCommunicator {
+    BeagleLeds leds;
     public:
-        XbeePollux(char* port) : XbeeCommunicator(port) { }
+        XbeePollux(char* port) : XbeeCommunicator(port) { 
+            leds.set_status_led();
+        }
 
         void run (XBeeFrame* frame) {
             //XbeeCommunicator::run(frame);
             switch (frame->api_id) {
                 case RX_PACKET:
+                    leds.set_rgb_led(BLUE);
                     printf("Do something with : '%s'\n", frame->content.rx.payload);
+                    usleep(1000000);
+                    leds.reset_rgb_led(BLUE);
                     break;
                 default:
+                    leds.set_rgb_led(RED);
+                    usleep(1000000);
+                    leds.reset_rgb_led(BLUE);
                     printf("Incoming frame that's not useful.\n");
+                    leds.reset_rgb_led(RED);
                     break;
             }
         }
@@ -30,12 +37,8 @@ class XbeePollux : public XbeeCommunicator {
 
 int main(void) {
     XbeePollux s = XbeePollux("/dev/ttyO2");
-    //Serial s = Serial("/dev/ttyO2");
 
-    //if (s.begin(B9600) == 0) {
     if (s.begin(panid,venid) >= 0) {
-        //char data[] = { 0x7e, 0x00, 0x04, 0x08, 0x52, 0x4D, 0x59, 0xFF };
-        //((Serial) s).write(data, 8);
         for (;;) 
             s.poll();
         return 0;
