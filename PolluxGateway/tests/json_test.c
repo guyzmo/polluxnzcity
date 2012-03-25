106294208fa553839f7972ec456aff7d230ac427
Guyzmo <guyzmo+github@m0g.net>
2012-03-25 05:26:58 +0200

    added measure iteration and hashmap creation

--- PolluxGateway/tests/json_test.c
+++ PolluxGateway/tests/json_test.c
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <iomanip>
 #include <iostream>
 #include <algorithm>
 #include <tr1/unordered_map>
@@ -36,7 +37,7 @@ bool cmdOptionExists(char** begin, char** end, const std::string& option)
 {
     return std::find(begin, end, option) != end;
 }
-/////////////////////
+///////////////////// Sensor
 
 class Sensor {
     std::string name;
@@ -81,33 +82,36 @@ class Action : public Sensor {
 ////////////////////
 
 class PolluxConfiguration {
-                    //  ZBaddr                      i2caddr         sensors
-    std::tr1::unordered_map<unsigned long, std::tr1::unordered_map<uint8_t, std::vector<Sensor>*> > sensors_map;
-                        // configopt    val
-    std::tr1::unordered_map<std::string, std::string> configuration_map;
-                     // datastore                       key             val
-    std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> > datastores_map;
+    std::tr1::unordered_map<unsigned long /* ZigBee Address */, std::tr1::unordered_map<uint8_t /* i2c addr */, std::vector<Sensor>/* sensor list */ > > sensors_map;
+    std::tr1::unordered_map<std::string /* target datastore */, std::tr1::unordered_map<std::string /* key */, std::string/* val */> > datastores_map;
+    std::tr1::unordered_map<std::string /* config option */   , std::string /* val */> configuration_map;
        
+    unsigned int meas_idx;
+    unsigned int stop;
+    std::tr1::unordered_map<uint16_t, std::tr1::unordered_map<uint8_t, std::vector<Sensor> >::iterator > module_iterator_map;
 
     public:
+        PolluxConfiguration() {
+            meas_idx=-1;
+            stop=0;
+        }
         int get_configuration(std::string& path) {
             std::ostringstream fname;
-            bool checked = false;
 
             fname << path << "config.json";
-            struct json_object *new_obj;
+            struct json_object *json_data;
 
-            new_obj = json_object_from_file((char*)fname.str().c_str());
-            if (is_error(new_obj)) {
+            json_data = json_object_from_file((char*)fname.str().c_str());
+            if (is_error(json_data)) {
                 std::cout<<"Invalid JSON file. Please check your configuration file."<<std::endl;
                 return -2;
             }
-            if (!json_object_has_key(new_obj,"configuration")) {
+            if (!json_object_has_key(json_data,"configuration")) {
                 std::cout<<"Missing 'configuration' token in configuration file."<<std::endl;
                 return -4;
             }
 
-            struct json_object* config = json_object_object_get(new_obj, "configuration");
+            struct json_object* config = json_object_object_get(json_data, "configuration");
 
             if (!json_object_has_key(config,"tty_port")) {
                 std::cout<<"Missing 'tty_port' token in configuration section."<<std::endl;
@@ -119,31 +123,33 @@ class PolluxConfiguration {
             }
 
 
-            printf("config.tty_port: %s\n", json_object_get_string(json_object_object_get(config,"tty_port")));
-            printf("config.wud_sleep_time: %s\n", json_object_get_string(json_object_object_get(config,"wud_sleep_time")));
+            json_object_object_foreach(config,key,value) {
+                configuration_map[key] = json_object_get_string(json_object_object_get(config,key));
+                std::cout<<"config."<<key<<": "<<json_object_get_string(json_object_object_get(config,key))<<std::endl;
+            }
 
-            free(new_obj);
+            free(json_data);
             return 1;
         }
 
         bool get_datastores(std::string& path) {
-            struct json_object *new_obj;
+            struct json_object *json_data;
             std::ostringstream fname;
 
             fname << path << "config.json";
 
-            new_obj = json_object_from_file((char*)fname.str().c_str());
-            if (is_error(new_obj)) {
+            json_data = json_object_from_file((char*)fname.str().c_str());
+            if (is_error(json_data)) {
                 std::cout<<"Invalid JSON file. Please check your configuration file syntax."<<std::endl;
                 return -2;
             }
         
-            if (!json_object_has_key(new_obj,"datastores")) {
+            if (!json_object_has_key(json_data,"datastores")) {
                 std::cout<<"Missing 'datastores' token in configuration file."<<std::endl;
                 return -4;
             }
 
-            struct json_object* datastores = json_object_object_get(new_obj, "datastores");
+            struct json_object* datastores = json_object_object_get(json_data, "datastores");
 
             json_object_object_foreach(datastores,name,datastore) {
                 if (!json_object_has_key(datastore,"post_url")) {
@@ -154,53 +160,106 @@ class PolluxConfiguration {
                     std::cout<<"Missing 'post_url' token in datastore "<<name<<" section."<<std::endl;
                     return -4;
                 }
-                printf("datastore(%s).post_url: '%s'\n", name, json_object_get_string(json_object_object_get(datastore,"post_url")));
-                printf("datastore(%s).api_key: '%s'\n", name, json_object_get_string(json_object_object_get(datastore,"api_key")));
+                json_object_object_foreach(datastore,key,value) {
+                    printf("datastore(%s).%s: '%s'\n", name, key, json_object_get_string(json_object_object_get(datastore,key)));
+                    datastores_map[name][key] = json_object_get_string(value);
+                }
             }
 
-            free(new_obj);
+            free(json_data);
             return 1;
         }
 
         int get_sensors(std::string& path) {
-           /* 
-    std::unordered_map<uint8_t, std::vector<Sensor>*> sensors_map;
-    std::vector<Sensor>* sensor;
-
-    sensor = new std::vector<Sensor>();
-    sensor->push_back(Sensor("Dust","ppm",0x26, 0));
-    sensors_map[0x26] = sensor;
-
-    sensor = new std::vector<Sensor>();
-    sensor->push_back(Action("Fan",0x27, 0));
-    sensor->push_back(Sensor("Noise level","dB",0x27, 1));
-    sensor->push_back(Sensor("Temperature","ºC", 0x27, 2));
-    sensors_map[0x27] = sensor;
-          */
-            struct json_object *new_obj;
+            struct json_object *json_data;
             std::ostringstream fname;
-            bool checked = false;
 
             fname << path << "sensors.json";
 
-            new_obj = json_object_from_file((char*)fname.str().c_str());
+            json_data = json_object_from_file((char*)fname.str().c_str());
 
-            new_obj = json_object_from_file((char*)fname.str().c_str());
-            if (is_error(new_obj)) {
+            json_data = json_object_from_file((char*)fname.str().c_str());
+
+            if (is_error(json_data)) {
                 std::cout<<"Invalid JSON file. Please check your configuration file syntax."<<std::endl;
                 return -2;
             }
             
-            json_object_object_foreach(new_obj,module_name,module) {
+            json_object_object_foreach(json_data,module_name,module) {
+                unsigned long int zb_address;
+                std::istringstream* ss = new std::istringstream( module_name );
+                (*ss) >> std::hex >> zb_address;
+                delete(ss);
+
+                // if sensor declaration has name, address and register declared
                 for (int i=0;i<json_object_array_length(module);++i) {
-                    json_object_object_foreach(json_object_array_get_idx(module,i),sensor_key,sensor_val) {
-                        printf("module(%s)->sensor(%d):	%s: %s\n", module_name, i, sensor_key, json_object_to_json_string(sensor_val));
+                    struct json_object* sensor_module = json_object_array_get_idx(module, i);
+
+                    if (json_object_has_key(sensor_module, "name") && json_object_has_key(sensor_module, "address") && json_object_has_key(sensor_module, "register")) {
+                        unsigned short int i2c_address, reg;
+                        ss = new std::istringstream( json_object_get_string(json_object_object_get(sensor_module, "address"))+2 );
+                        (*ss) >> std::hex >> i2c_address;
+                        delete(ss);
+                        // check if the address has correctly been casted to int
+                        ss = new std::istringstream( json_object_get_string(json_object_object_get(sensor_module, "register")) );
+                        (*ss) >> std::hex >> reg;
+                        delete(ss);
+                        // if it is a Sensor module (i.e. that has a 'unit' key)
+                        if (json_object_has_key(sensor_module, "unit")) {
+                            std::cout<<"module(0x"<<std::setw(16)<<std::setfill('0')<<std::hex<<zb_address<<":0x"<<i2c_address<<") := sensor("\
+                                                                                                       <<json_object_get_string(json_object_object_get(sensor_module,"name"))<<","\
+                                                                                                       <<json_object_get_string(json_object_object_get(sensor_module,"unit"))\
+                                                                                                       <<",0x"<<std::hex<<i2c_address<<",0x"<<std::hex<<reg<<")"<<std::endl;
+                            sensors_map[zb_address][i2c_address].push_back(Sensor(json_object_get_string(json_object_object_get(sensor_module,"name")),
+                                        json_object_get_string(json_object_object_get(sensor_module,"unit")),
+                                        i2c_address, reg));
+                            // if it is an Action module (i.e. that has *no* 'unit' key)
+                        } else  {
+                            sensors_map[zb_address][i2c_address].push_back(Action(json_object_get_string(json_object_object_get(sensor_module,"name")),
+                                        i2c_address, reg));
+                            std::cout<<"module(0x"<<std::setw(16)<<std::setfill('0')<<std::hex<<zb_address<<":0x"<<i2c_address<<") := action("\
+                                                                                                       <<json_object_get_string(json_object_object_get(sensor_module,"name"))\
+                                                                                                       <<",0x"<<std::hex<<i2c_address<<",0x"<<std::hex<<reg<<")"<<std::endl;
+                        }
                     }
+                    module_iterator_map[zb_address] = sensors_map[zb_address].begin();
                 }
+
+                free(json_data);
+                return 1;
+            }
+        }
+        
+
+        char* get_next_measure(unsigned long int module) {
+            std::tr1::unordered_map<uint8_t, std::vector<Sensor> >::iterator& sensor_it = module_iterator_map[module];
+            char* buf = (char*)malloc(sizeof(char)*3);
+            buf[0] = 0x0;
+            buf[1] = 0x0;
+            buf[2] = 0x0;
+
+            if (sensor_it->second.size() > 1 and stop == 0) {
+                stop = sensor_it->second.size()-2;
+                meas_idx = 0;
+            } else if (meas_idx < stop) {
+                ++meas_idx;
+                return buf;
+            } else if (meas_idx == stop) {
+                stop = 0;
+                meas_idx = -1;
+                return buf;
             }
 
-            free(new_obj);
-            return 1;
+            buf[0] = 0x42;
+            buf[1] = (unsigned short int)(*sensor_it).first;
+            buf[2] = (unsigned short int)(*sensor_it).second.size();
+
+            ++sensor_it;
+            if (sensor_it == sensors_map[module].end()) {
+                sensor_it = sensors_map[module].begin();
+            }
+
+            return buf;
         }
 };
 
@@ -223,6 +282,16 @@ int main(int argc, char * argv[])
         std::cout<<"Configuration file not found. Can't get datastores"<<std::endl;
     if (!pconfig.get_sensors(filename))
         std::cout<<"Sensor description file not found."<<std::endl;
+
+    char* buf;
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
+    buf = pconfig.get_next_measure(0x0013a20040698679); std::cout<<"sending: "<<std::hex<<(int)buf[0]<<","<<std::hex<<(int)buf[1]<<","<<std::hex<<(int)buf[2]<<std::endl;delete(buf);
 
     return 0;
 }
